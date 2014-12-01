6a2ed2c227b98bf3f47c6836ab05e495ce7c4596
Vitaliy Chorniy <chornij1@gmail.com>
2014-12-01 22:46:46 +0200

    increased the route, made popup on ented buildings, created empty pages for quiz

--- js/human.js
+++ js/human.js
@@ -1,5 +1,109 @@
 
 
+zIndexCheck = function(position) {		//Z-index controll, visibility of
+										//main caracter in front and behind
+										//of the buildings
+	var index;
+	if(position > 250) {
+		index =  1;
+	} else if(position < 250){
+		index = 0;
+	}
+	if(position > 400) {
+		index = 2;
+	} else if(position < 400 && position > 330){
+		index = 1;
+	}
+	if(position > 500) {
+		index = 3;
+	} else if(position < 500 && position > 400){
+		index = 2;
+	}
+	return index;
+};
+
+
+function confirmStartQuiz(name) {									//this function fires when enterBuilding
+																	//function indicates that the hero entered
+																	//the bulding
+	
+	//before showing the popup we fire this function
+	//it disables the keyboard, for user not to make
+	//unnessasary steps, it inserts needed question in to the
+	//popup and button with approprite link to required quiz :
+
+	$('#gameConfirm').on('show.bs.modal', function (e) {
+		document.body.setAttribute("onkeydown","return false");						//disable keyboard
+		var title = document.getElementById('gameConfirmTitle');
+		title.innerHTML = 'do you want to start ' + name + ' quiz?';
+
+		var startButton = document.getElementById('confirmStart');
+		startButton.setAttribute('href','#' + name + 'Quiz');
+	});
+
+	//this function enables the keyboard
+	//after pop up is closed
+	$('#gameConfirm').on('hide.bs.modal', function (e) {
+
+		document.body.setAttribute("onkeydown","mainDude.move(event.keyCode,5);");	//enable keyboard
+	});
+
+	//this function opens the popup, after
+	//above configs are setteled
+	$('#gameConfirm').modal();
+
+	//after popup is closed we move the main hero 30px lover
+	//to make him visible
+	mainDude.humanBody.stepsTop += 30;
+	mainDude.humanBody.human.style.top = mainDude.humanBody.stepsTop + "px";
+}
+
+
+function enterBuilding(id) {
+	var dudePos = $(id).offset();
+
+	var htmlBuildingPos = $("#htmlBuilding").offset();
+	var cssBuildingPos = $("#cssBuilding").offset();
+	var jsBuildingPos = $("#javascriptBuilding").offset();
+	var jqBuildingPos = $("#jqueryBuilding").offset();
+	var oopBuildingPos = $("#oopBuilding").offset();
+
+	if(dudePos.top > htmlBuildingPos.top + 100 && dudePos.left < htmlBuildingPos.left + 250 &&
+		dudePos.top < htmlBuildingPos.top + 150) {
+
+			confirmStartQuiz('html');
+
+		}
+	if(dudePos.top > cssBuildingPos.top + 100 && dudePos.left > cssBuildingPos.left &&
+		dudePos.top < cssBuildingPos.top + 150) {
+
+			confirmStartQuiz('css');
+
+		}
+	if(dudePos.top > jsBuildingPos.top + 100 && dudePos.left > jsBuildingPos.left &&
+		dudePos.left < jsBuildingPos.left + 150 &&
+		dudePos.top < jsBuildingPos.top + 280) {
+
+			confirmStartQuiz('js');
+			
+		}
+	if(dudePos.top > oopBuildingPos.top + 140 && dudePos.left < oopBuildingPos.left + 250 &&
+		dudePos.top < oopBuildingPos.top + 190) {
+
+			confirmStartQuiz('oop');
+			
+		}
+	if(dudePos.top > jqBuildingPos.top + 80 && dudePos.left > jqBuildingPos.left &&
+		dudePos.top < jqBuildingPos.top + 130) {
+
+			confirmStartQuiz('jq');
+	
+		}
+}
+
+
+
+
 
 
 function brain() {
@@ -12,13 +116,10 @@ function brain() {
 
 function body() {
 
-	this.human = document.createElement('div');						
-	this.human.style.top = "130px";								//Start position top
-	this.human.style.left = "730px";
-	this.human.style.zIndex = 0;							//Start position left
-
-	this.stepsRight = 730;
-	this.stepsTop = 100;
+	this.human = document.createElement('div');
+	this.human.style.zIndex = 0;
+	this.stepsRight = 0;
+	this.stepsTop = 0;
 	var block = document.getElementById('gamePage');
 	block.appendChild(this.human);
 
@@ -33,18 +134,12 @@ Human.prototype.move = function(direction, speed) {
 
 		var dudePos = $("#mainGuy").offset();
 
-		var htmlBuildingPos = $("#htmlBuilding").offset();
-		var cssBuildingPos = $("#cssBuilding").offset();
-		var jsBuildingPos = $("#javascriptBuilding").offset();
-		var jqBuildingPos = $("#jqueryBuilding").offset();
-		var oopBuildingPos = $("#oopBuilding").offset();
-
 		if(direction == 39) {												        // if "->" button is pressed
 			this.humanBody.stepsRight += speed;
 			this.humanBody.human.style.left = this.humanBody.stepsRight + "px";
 			this.humanBody.human.style.background = "url(img/people/walker_right.gif)";
-			if(dudePos.left > 1250) {											    // prevent the car from moving out from the
-				this.humanBody.stepsRight = 1150;									// main block on right side
+			if(dudePos.left > 1250) {											   
+				this.humanBody.stepsRight = 1150;									
 			}
 			return;
 		}
@@ -52,8 +147,8 @@ Human.prototype.move = function(direction, speed) {
 			this.humanBody.stepsRight -= speed;
 			this.humanBody.human.style.left = this.humanBody.stepsRight + "px";
 			this.humanBody.human.style.background = "url(img/people/walker_left.gif)";
-			if(dudePos.left < 100) {												// prevent the car from moving out from the
-				this.humanBody.stepsRight = 0;										// main block on left side
+			if(dudePos.left < 100) {
+				this.humanBody.stepsRight = 0;
 			}
 			return;
 		}
@@ -61,29 +156,11 @@ Human.prototype.move = function(direction, speed) {
 			this.humanBody.stepsTop += speed;
 			this.humanBody.human.style.top = this.humanBody.stepsTop + "px";
 			this.humanBody.human.style.background = "url(img/people/walker_down.gif)";
-			if(dudePos.top > 600) {												// prevent the car from moving lover than
-				this.humanBody.stepsTop = 600;									// 500px from top
+			if(dudePos.top > 600) {
+				this.humanBody.stepsTop = 600;
 			}
 
-			//Z-index controll, visibility of
-			//main caracter in front and on te back
-			//of the buildings
-			if(dudePos.top > 250) {
-				this.humanBody.human.style.zIndex = 1;
-			} else if(dudePos.top < 330){
-				this.humanBody.human.style.zIndex = 0;
-			}
-			if(dudePos.top > 400) {
-				this.humanBody.human.style.zIndex = 2;
-			} else if(dudePos.top < 400 && dudePos.top > 330){
-				this.humanBody.human.style.zIndex = 1;
-			}
-			if(dudePos.top > 510) {
-				this.humanBody.human.style.zIndex = 3;
-			} else if(dudePos.top < 470 && dudePos.top > 400){
-				this.humanBody.human.style.zIndex = 2;
-			}
-
+			this.humanBody.human.style.zIndex = zIndexCheck(dudePos.top);		//set z-index 
 
 			return;
 		}
@@ -91,65 +168,13 @@ Human.prototype.move = function(direction, speed) {
 			this.humanBody.stepsTop -= speed;
 			this.humanBody.human.style.top = this.humanBody.stepsTop + "px";
 			this.humanBody.human.style.background = "url(img/people/walker_up.gif)";
-			if(dudePos.top < 50) {												// prevent the car from moving higher than
-				this.humanBody.stepsTop = 50;										// 50px from top
-			}
-			if(dudePos.top > htmlBuildingPos.top + 100 && dudePos.left < htmlBuildingPos.left + 250 &&
-				dudePos.top < htmlBuildingPos.top + 150) {
-
-				alert("html here!")
-				//Open popup here!
-
-			}
-			if(dudePos.top > cssBuildingPos.top + 100 && dudePos.left > cssBuildingPos.left &&
-				dudePos.top < cssBuildingPos.top + 150) {
-
-				alert("css here!")
-				//Open popup here!
-
-			}
-			if(dudePos.top > jsBuildingPos.top + 100 && dudePos.left > jsBuildingPos.left &&
-				dudePos.left < jsBuildingPos.left + 150 &&
-				dudePos.top < jsBuildingPos.top + 280) {
-
-				alert("js here!")
-				//Open popup here!
-
-			}
-			if(dudePos.top > oopBuildingPos.top + 140 && dudePos.left < oopBuildingPos.left + 250 &&
-				dudePos.top < oopBuildingPos.top + 190) {
-
-				alert("oop here!")
-				//Open popup here!
-
-			}
-			if(dudePos.top > jqBuildingPos.top + 80 && dudePos.left > jqBuildingPos.left &&
-				dudePos.top < jqBuildingPos.top + 130) {
-
-				alert("jQuery here!")
-				//Open popup here!
-
+			if(dudePos.top < 50) {												
+				this.humanBody.stepsTop = 50;										
 			}
 
-			//Z-index controll, visibility of
-			//main caracter in front and on te back
-			//of the buildings
-			if(dudePos.top > 250) {
-				this.humanBody.human.style.zIndex = 1;
-			} else if(dudePos.top < 330){
-				this.humanBody.human.style.zIndex = 0;
-			}
-			if(dudePos.top > 400) {
-				this.humanBody.human.style.zIndex = 2;
-			} else if(dudePos.top < 400 && dudePos.top > 330){
-				this.humanBody.human.style.zIndex = 1;
-			}
-			if(dudePos.top > 510) {
-				this.humanBody.human.style.zIndex = 3;
-			} else if(dudePos.top < 470 && dudePos.top > 400){
-				this.humanBody.human.style.zIndex = 2;
-			}
+			enterBuilding("#mainGuy");
 
+			this.humanBody.human.style.zIndex = zIndexCheck(dudePos.top);		//set z-index 
 
 			return;
 		}
@@ -160,12 +185,19 @@ Human.prototype.stop = function() {
 	this.humanBody.human.style.background = "url(img/people/walker.png)";
 }
 
+Human.prototype.study = function(name, points) {
+	if(name == "js"){
 
+	}
+}
 
 var mainDude = new Human();														
 			
 mainDude.humanBody.human.setAttribute("id","mainGuy");	
-mainDude.humanBody.stepsTop += 50;	
+mainDude.humanBody.human.style.top = "100px";
+mainDude.humanBody.human.style.left = "730px";
+mainDude.humanBody.stepsTop = 100;
+mainDude.humanBody.stepsRight = 730;
 
 var gameArea = document.getElementById('gamePage');
 document.body.setAttribute("onkeydown","mainDude.move(event.keyCode,5);")
