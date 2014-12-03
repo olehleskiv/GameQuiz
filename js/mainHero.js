c93ed54b1c214b62d9cef015d59eb83b569fd080
Vitaliy Chorniy <chornij1@gmail.com>
2014-12-03 20:36:06 +0200

    added test study buttons in mainDude

--- js/mainHero.js
+++ js/mainHero.js
@@ -13,6 +13,61 @@ define(['Human'], function(Human) {
 	document.body.setAttribute("onkeydown","mainDude.move(event.keyCode,5);");
 	document.body.setAttribute("onkeyup", "mainDude.stop();");
 	
+
+
+			//test stydy buttons should be deleted
+			var button = document.createElement('button');
+			button.setAttribute('class','testButton');
+			button.innerHTML = "study html";
+
+
+			button.onclick = function() {
+				mainDude.study('html', 5);
+			}
+			gameArea.appendChild(button);
+
+			var button = document.createElement('button');
+			button.setAttribute('class','testButton1');
+			button.innerHTML = "study css";
+
+
+			button.onclick = function() {
+				mainDude.study('css', 5);
+			}
+			gameArea.appendChild(button);
+
+			var button = document.createElement('button');
+			button.setAttribute('class','testButton2');
+			button.innerHTML = "study js";
+
+
+			button.onclick = function() {
+				mainDude.study('js', 5);
+			}
+			gameArea.appendChild(button);
+
+			var button = document.createElement('button');
+			button.setAttribute('class','testButton3');
+			button.innerHTML = "study jq";
+
+
+			button.onclick = function() {
+				mainDude.study('jq', 5);
+			}
+			gameArea.appendChild(button);
+
+			var button = document.createElement('button');
+			button.setAttribute('class','testButton4');
+			button.innerHTML = "study oop";
+
+
+			button.onclick = function() {
+				mainDude.study('oop', 5);
+			}
+			gameArea.appendChild(button);
+
+
+
 	return mainDude;
 	
 });
