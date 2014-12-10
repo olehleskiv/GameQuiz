4869b139a22b2f279556284617099f958626454a
Vitaliy Chorniy <chornij1@gmail.com>
2014-12-10 04:23:58 +0200

    added htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
 		      include: ['build'],
 		      baseUrl: "app/js",
 		      mainConfigFile: "app/js/build.js",
-		      out: "prod/js/main.min.js"
+		      out: "prod/js/main.js"
 		    }
 		  }
 		},
@@ -44,7 +44,27 @@ module.exports = function(grunt) {
 		  }
 		},
 
+		htmlmin: {                                       // Task
+		    multiple: { 
+
+		     options: {                                 // Target options
+		        removeComments: true,
+		        collapseWhitespace: true
+		      },                                 // Target
+		      files: [{                                  // Dictionary of files
+		        expand: true,
+		        cwd: 'app/',                             // Project root
+		        src: '**/*.html',                        // Source
+		        dest: 'prod/'                            // Destination
+		      }]
+		    }
+		},
+
 		watch: {
+			html: {
+				files: ['app/*.html'],
+				tasks: ['htmlmin']
+			},
 		    scripts: {
 		        files: ['app/js/*.js','app/js/cont/*.js','app/js/libs/*.js','app/js/models/*.js','app/js/views/*.js' ],
 		        tasks: ['requirejs'],
@@ -60,7 +80,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
 	
-	grunt.registerTask('default', ['requirejs', 'imagemin', 'less', 'watch' ]);
+	grunt.registerTask('default', ['htmlmin', 'requirejs', 'imagemin', 'less', 'watch' ]);
 };
