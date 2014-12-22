0402f5d000b27e62801dcc5dc812d60db558eabe
Vitaliy Chorniy <chornij1@gmail.com>
2014-12-22 18:51:49 +0200

    result page

--- app/js/libs/facebook.js
+++ app/js/libs/facebook.js
@@ -5,16 +5,22 @@
 			$('#share_button').click(function(e){
 				e.preventDefault();
 
-				this.htmlpoints = localStorage.getItem('htmlPoints');
-
 				FB.ui({
 					method: 'feed',
 					name: 'LearnFrontEnd',
 					link: 'http://learnfrontend.tk',
-					picture: 'http://learnfrontend.tk/img/badges/htmlBadgeMiddle.png',
-					caption: 'LearnFrontEnd',
-					description: 'I just got' + this.htmlpoints + 'points',
-					message: ''
+					picture: 'http://learnfrontend.tk/img/badges/jqBadgeMaster.png',
+					caption: 'Learn FrontEnd Quiz!',
+
+					description: 'I just passed Front End quiz!, my results:\n' + 
+					'HTML ' + localStorage.getItem('htmlPoints') + ' points\n' + 
+					'CSS: ' + localStorage.getItem('cssPoints') + ' points\n' +
+					'JavaScript: ' + localStorage.getItem('jsPoints') + ' points\n' + 
+					'Jquery: ' + localStorage.getItem('jqPoints') + ' points\n' +
+					'OOP: ' + localStorage.getItem('jqPoints') + ' points',
+
+
+					message: 'Try it your self!'
 				});
 			});
 	});
